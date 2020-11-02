8a5dec6f6819cf341cde76cf54c4a5d9d22da8df
Dim <dmitriy.saienko@gmail.com>
2020-11-02 21:12:44 +0200

    trying to fix peerjs

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,7 +6,7 @@ class Chat {
 
     constructor(socket) {
         this.socket = socket;
-        this.peer = new Peer(undefined, {host: location.hostname, port: 9000, path: '/peerjs'});
+        this.peer = new Peer(undefined, {host: 'peerjs-d.herokuapp.com', port: 443, secure: true});
         this.UI.userVideo.muted = true;
 
         this.peer.on('open', peerId => {
